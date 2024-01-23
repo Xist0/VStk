015f5cec94ac8d07a3a8ef7ed2473b69a9639564
Xist0 <x1spridcherd@gmail.com>
2024-01-23 23:15:40 +0500

    1

--- src/components/AuthLink.jsx
+++ src/components/AuthLink.jsx
@@ -2,43 +2,43 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function AuthLink() {
-  const [verificationCode, setVerificationCode] = useState('');
-  const [error, setError] = useState('');
+    const [verificationCode, setVerificationCode] = useState('');
+    const [error, setError] = useState('');
 
-  // Проверка кода при авторизации
-  const authorize = async () => {
-    try {
-      const response = await axios.post('https://localhost:3000/authorize', { verificationCode }, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+    // Проверка кода при авторизации
+    const authorize = async () => {
+        try {
+            const response = await axios.post('https://localhost:3000/authorize', { verificationCode }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
 
-      // Если авторизация успешна, перенаправляем пользователя на главную страницу
-      if (response.status === 200) {
-        window.location.href = '/auth';
-      } else {
-        setError('Unauthorized');
-      }
-    } catch (error) {
-      setError('Не верный код доступа');
-      console.error('Authorization error:', error);
-    }
-  };
+            // Если авторизация успешна, перенаправляем пользователя на главную страницу
+            if (response.status === 200) {
+                window.location.href = '/auth';
+            } else {
+                setError('Unauthorized');
+            }
+        } catch (error) {
+            setError('Не верный код доступа');
+            console.error('Authorization error:', error);
+        }
+    };
 
-  return (
-    <div>
-      <h1> Аунтификация</h1>
-      <div>
-        <label>Код доступа:</label>
-        <input type="text" value={verificationCode} onChange={(e) => setVerificationCode(e.target.value)} />
-      </div>
-      <div>
-        <button onClick={authorize}>Ввести</button>
-      </div>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-    </div>
-  );
+    return (
+        <div className="components">
+            <h1> Аунтификация</h1>
+            <div >
+                <label>Код доступа:</label>
+                <input type="text" value={verificationCode} onChange={(e) => setVerificationCode(e.target.value)} />
+            </div>
+            <div>
+                <button onClick={authorize}>Ввести</button>
+            </div>
+            {error && <p style={{ color: 'red' }}>{error}</p>}
+        </div>
+    );
 }
 
 export default AuthLink;
